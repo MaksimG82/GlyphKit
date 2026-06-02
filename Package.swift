66024ed585da803cd5d1632f98fdc5c143f1bb22
Maksim Gaisin <maksim@gaisin.space>
2026-06-02 14:17:10 +0200

    package updated

--- Package.swift
+++ Package.swift
@@ -1,27 +1,18 @@
-// swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "GlyphKit",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "GlyphKit",
-            targets: ["GlyphKit"]
-        ),
+        .library(name: "GlyphKit", targets: ["GlyphKit"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "GlyphKit"
-        ),
-        .testTarget(
-            name: "GlyphKitTests",
-            dependencies: ["GlyphKit"]
-        ),
+        .target(name: "GlyphKit"),
+        .testTarget(name: "GlyphKitTests", dependencies: ["GlyphKit"])
     ],
     swiftLanguageModes: [.v6]
 )
